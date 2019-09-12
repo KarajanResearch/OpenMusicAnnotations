89d3241be696967c94b26d347c60fe5c1376580e
Martin Aigner <martin@maigner.net>
2019-09-12 11:18:09 +0200

    adding score file command and views

---
+++ grails-app/controllers/oma/AbstractMusicPartScoreFileCommand.groovy
@@ -0,0 +1,29 @@
+package oma
+
+import grails.validation.Validateable
+import org.springframework.web.multipart.MultipartFile
+
+class AbstractMusicPartScoreFileCommand implements Validateable {
+
+    MultipartFile scoreFile
+    Long abstractMusicPartId
+    Integer version
+
+
+    static constraints = {
+        abstractMusicPartId nullable: false
+        version nullable: false
+        scoreFile  validator: { val, obj ->
+            if ( val == null ) {
+                return false
+            }
+            if ( val.empty ) {
+                return false
+            }
+
+            ['pdf', "pdf"].any { extension ->
+                val.originalFilename?.toLowerCase()?.endsWith(extension)
+            }
+        }
+    }
+}
