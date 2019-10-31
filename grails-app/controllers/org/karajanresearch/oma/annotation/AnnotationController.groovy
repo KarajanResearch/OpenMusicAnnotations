706a551687c33da6a3c33a7e9ae5c4ae8bfa55b7
Martin Aigner <martin@maigner.net>
2019-10-31 09:07:11 +0100

    access control to controllers

--- grails-app/controllers/org/karajanresearch/oma/annotation/AnnotationController.groovy
+++ grails-app/controllers/org/karajanresearch/oma/annotation/AnnotationController.groovy
@@ -1,8 +1,10 @@
 package org.karajanresearch.oma.annotation
 
+import grails.plugin.springsecurity.annotation.Secured
 import grails.validation.ValidationException
 import static org.springframework.http.HttpStatus.*
 
+@Secured(["ROLE_ADMIN"])
 class AnnotationController {
 
     static scaffold = Annotation
