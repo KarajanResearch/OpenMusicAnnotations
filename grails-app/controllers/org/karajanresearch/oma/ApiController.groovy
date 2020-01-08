30ba725a9b6b933bfde66642e8bc369488b7d4e9
Martin Aigner <martin@maigner.net>
2020-01-08 15:10:07 +0100

    adding api for adding composers

--- grails-app/controllers/org/karajanresearch/oma/ApiController.groovy
+++ grails-app/controllers/org/karajanresearch/oma/ApiController.groovy
@@ -4,7 +4,7 @@ import grails.converters.JSON
 import grails.gorm.transactions.ReadOnly
 import grails.plugin.springsecurity.annotation.Secured
 import org.karajanresearch.oma.api.AnnotationApiService
-import org.karajanresearch.oma.music.Recording
+import org.karajanresearch.oma.api.ComposerApiService
 
 /**
  * oma.cloud/api/
@@ -16,6 +16,31 @@ class ApiController {
     // TODO: add api call to add recordings.
     // apply to spreadsheet!
     // perform import in jupyter
+
+    ComposerApiService composerApiService
+
+    /**
+     * requires param.method to select what to call in composerApiService
+     * @return
+     */
+    def composer() {
+        println "composer"
+        println params
+
+        def r = [error: "not implemented"]
+
+        switch (params["method"]) {
+            case "add":
+                r = composerApiService.addComposer(params)
+                break
+        }
+
+        render r as JSON
+    }
+
+
+
+
 
     AnnotationApiService annotationApiService
 
