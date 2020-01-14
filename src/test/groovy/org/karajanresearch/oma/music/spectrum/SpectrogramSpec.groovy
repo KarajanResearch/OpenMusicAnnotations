8131cd68d98fa495491a02f84a650372503322c3
Martin Aigner <martin@maigner.net>
2020-01-14 14:55:22 +0100

    visual feature extraction

---
+++ src/test/groovy/org/karajanresearch/oma/music/spectrum/SpectrogramSpec.groovy
@@ -0,0 +1,18 @@
+package org.karajanresearch.oma.music.spectrum
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class SpectrogramSpec extends Specification implements DomainUnitTest<Spectrogram> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/org/karajanresearch/oma/music/spectrum/SpectrumSampleWindowSpec.groovy
@@ -0,0 +1,18 @@
+package org.karajanresearch.oma.music.spectrum
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class SpectrumSampleWindowSpec extends Specification implements DomainUnitTest<SpectrumSampleWindow> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
