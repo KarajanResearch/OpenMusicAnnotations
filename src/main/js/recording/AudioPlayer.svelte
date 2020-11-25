b4a9f34926955c992788cfc20b8a8ed775dade05
Martin Aigner <martin@maigner.net>
2020-11-25 13:47:26 +0100

    toggle play/pause events through components

--- src/main/js/recording/AudioPlayer.svelte
+++ src/main/js/recording/AudioPlayer.svelte
@@ -1,40 +1,50 @@
 <script>
     import { onMount } from "svelte";
-    import Peaks from 'peaks.js';
 
     // id of the grails recording object passed from the outside
     export let recordingId;
 
-    onMount(async () => {
+    let audioElement = {};
 /*
-        const res = await fetch("/recording/ajaxGet/"+recordingId);
-        const ajaxData = await res.json();
-        recording = ajaxData.recording;
-        console.log(recording);
+    $: if(paused) {
+        audioElement.pause();
+    }
+    $: if(!paused) {
+        audioElement.play();
+    }
+*/
 
- */
-        const options = {
-            containers: {
-                overview: document.getElementById('overview-container'),
-                zoomview: document.getElementById('zoomview-container')
-            },
-            mediaElement: document.querySelector('audio'),
-            dataUri: '/recording/getPeaksFile/' + recordingId
+
+    let paused = true;
+
+    onMount(async () => {
+
+        audioElement = document.getElementById('audio_element_' + recordingId);
+        window.onTogglePaused = () => {
+            console.log("Audioplayer: " + paused);
+            if (paused) {
+                audioElement.play();
+            } else {
+                audioElement.pause();
+            }
+            paused = !paused;
         };
-        Peaks.init(options, function(err, peaks) {
-            // ...
-        });
+
+
+
     });
+
+
+
 
 
 
 </script>
 
 
-<audio controls preload="auto">
+<audio id="audio_element_{recordingId}" controls preload="auto">
     <source src="/recording/getAudioFile/{recordingId}" type="audio/wav" />
     <track src="" kind="captions" />
         Your browser does not support the audio element.
 </audio>
-<div id="overview-container"></div>
-<div id="zoomview-container"></div>
+Audio player paused: {paused}
--- src/main/js/recording/RecordingUi.svelte
+++ src/main/js/recording/RecordingUi.svelte
@@ -1,21 +1,51 @@
 <script>
     import { onMount } from "svelte";
+    import { createEventDispatcher } from 'svelte';
     import AudioPlayer from "./AudioPlayer.svelte";
+    import DynamicWaveForm from "./DynamicWaveForm.svelte";
 
     // id of the grails recording object passed from the outside
     export let recordingId;
 
     // recording data structure loaded onMount
-    let recording = {}
+    let recording = {};
+
+    const dispatch = createEventDispatcher();
+
+    function forward(event) {
+        dispatch('message', event.detail);
+    }
 
     onMount(async () => {
         const res = await fetch("/recording/ajaxGet/"+recordingId);
         const ajaxData = await res.json();
         recording = ajaxData.recording;
-        console.log(recording);
     });
 
+
+
+    /**
+     * keyboard input on the waveform. used for pausing, tapping, annotation...
+     * @param event
+     */
+    let key;
+    let keyCode;
+    function handleKeydown(event) {
+        key = event.key;
+        keyCode = event.keyCode;
+
+        if(keyCode == 32) {
+            console.log("space bar");
+            // bound t0 window in sub-component
+            onTogglePaused();
+
+        }
+    }
+
 </script>
+
+<svelte:window on:keydown={handleKeydown}/>
+
 
 
 <p>This is recording: {recordingId}</p>
@@ -29,6 +59,11 @@
     {/if}
 </p>
 
-<AudioPlayer recordingId="{recordingId}"/>
+<AudioPlayer recordingId={recordingId} />
 
+<DynamicWaveForm recordingId={recordingId} />
 
+<br/>
+Key: {key}
+<br />
+KeyCode: {keyCode}
